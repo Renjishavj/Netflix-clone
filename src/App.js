81af44a89a387088656b9e8b23c9a82405673e95
Renjishavj <renjishavj@gmail.com>
2024-01-28 00:22:28 +0530

    add more movies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {action,originals,horror} from "./Url"
+import {action,originals,horror,trending, comedy,romantic, documentries} from "./Url"
 import NavBar from './Components/NavBar/NavBar'
 import Banner from './Components/Banner/Banner'
 import RowPost from './Components/RowPost/RowPost'
@@ -10,8 +10,12 @@ function App() {
       <NavBar/>
       <Banner/>
      <RowPost url={originals} title='Netflix Originals'/>
+     <RowPost url={trending} title='Netflix Trendings'/>
      <RowPost url={action} title='Action Movies' isSmall/>
+     <RowPost url={romantic} title='Romantic Movies' isSmall/>
      <RowPost url={horror} title='Horror Movies' isSmall/>
+     <RowPost url={comedy} title='Comedy Movies' isSmall/>
+     <RowPost url={documentries} title='Documentries' isSmall/>
     </div>
   )
 }
